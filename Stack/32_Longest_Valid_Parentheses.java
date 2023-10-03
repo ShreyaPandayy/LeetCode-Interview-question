e763240f004345512f531bc63822fea5ba14bc4b
Mukul <mukul1singh23@gmail.com>
2023-10-03 20:07:24 +0530

    leetcode 32

---
+++ Stack/32_Longest_Valid_Parentheses.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        
+        
+        Stack<Character> ch = new Stack<Character>();
+        Stack<Integer> index = new Stack<Integer>();
+        index.push(-1);
+        int ans=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(')
+            {
+                ch.push(s.charAt(i));
+                index.push(i);
+
+            }
+            else
+            {
+                if(ch.isEmpty())
+                {
+
+                    index.push(i);
+                }
+                else
+                {
+                    if(ch.peek()=='(')
+                    ch.pop();
+                    index.pop();
+                    ans=Math.max(ans,i-index.peek());
+                }
+            }
+        }
+        return ans;
+    }
+}
